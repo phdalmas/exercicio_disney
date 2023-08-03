fbf2dd5662a3216380accd8536bf50f93976ff35
Pedro Dalmas <phdalforno@gmail.com>
2023-08-03 10:47:06 -0300

    adicionando FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     
     for (let i = 0; i<buttons.length; i++) {
@@ -14,8 +15,18 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    for (let i=0; i<questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFecha);
+    }
+
 })
 
+function abreOuFecha (elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo () {
     const buttons = document.querySelectorAll('[data-tab-button]');
